91b2277cbb049a3d2938b67ef929734c86ded6e5
chino <sarachoesteban@gmail.com>
2024-08-31 23:27:48 -0300

    Cambio metodo POST por GET

--- index.php
+++ index.php
@@ -38,17 +38,17 @@ You should have received a copy of the GNU General Public License along with thi
 
 <!-- Formulario busqueda -->
   <search class="px-3 pt-5">
-    <form name="formlink" method="post" class="form-inline" action="index.php" id="busqueda">
+    <form name="formlink" method="GET" class="form-inline" action="index.php" id="busqueda">
       <div class="input-group">
         <input type="text" class="form-control" name="query" placeholder="Buscar planito" required autofocus>
-        <input class="btn btn-dark" type="submit" name="Submit" value="&hearts;">
+        <input class="btn btn-dark" type="submit" value="&hearts;">
       </div>
     </form>
   </search>
 
 <!-- Categorías -->
   <div class="px-3 pt-4 text-center">
-  <form name="categorias" method="post" action="index.php" id="categorias">
+  <form name="categorias" method="GET" action="index.php" id="categorias">
     <button type="submit" name="cat" value="MAQ" class="btn btn-link btn-sm">Maquetas</button>
     <button type="submit" name="cat" value="BAR" class="btn btn-link btn-sm">Barriletes</button>
     <button type="submit" name="cat" value="MG" class="btn btn-link btn-sm">Motor a Goma</button>
@@ -138,19 +138,19 @@ function searchQuery($search, $file) : bool {
 
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
 
-  //Se guarda lo ingresado en la búsqueda
-  $queryString = date(DATE_RFC1123) . " " . $_POST["query"] . "\n";
+  //Se guarda lo ingresado en la búsqueda(log)
+  /* $queryString = date(DATE_RFC1123) . " " . $_GET["query"] . "\n";
   $logQueryFile = "/busquedas-log.txt";
-  file_put_contents(__DIR__ . $logQueryFile, $queryString , FILE_APPEND);
+  file_put_contents(__DIR__ . $logQueryFile, $queryString , FILE_APPEND); */
 
   $noResults = true;
  
-  if (array_key_exists("cat", $_POST)) {
+  if (array_key_exists("cat", $_GET)) {
 
-    $path = "db/" . $_POST["cat"] . ".json";
+    $path = "db/" . $_GET["cat"] . ".json";
     $data = file_get_contents($path);
     $json = json_decode($data);
     $dir = [...$json[0]->contents];
@@ -164,9 +164,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
 
-  } elseif (array_key_exists("query", $_POST)) {
+  } elseif (array_key_exists("query", $_GET)) {
 
-      $search = getQuery($_POST["query"]);
+      $search = getQuery($_GET["query"]);
       $data = file_get_contents("db/ALL.json");
       $json = json_decode($data);
       $dir = [...$json[0]->contents];
